442862ebccf5b115431c7e710322e3a195703ed1
lucasbarrosomk6 <lucasbarrosomk6@gmail.com>
2019-10-01 22:27:49 -0400

    ADSFADFASDF

--- Code Projects/wordLength.js
+++ Code Projects/wordLength.js
@@ -1,9 +1,12 @@
 let MaxWordLength = {};
-let str = "hot diggity dog!!!!!!!"
-function longestWord(str){
-    var arr = str.match(/[a-z,0-9]+/gi);
-   var sorted = arr.sort(function(a,b){return b.length-a.length});
-    return sorted[0];
+let str = "hot diggity dog!!!!!!!";
+function longestWord(str) {
+  var arr = str.match(/[a-z,0-9]+/gi);
+  var sorted = arr.sort(function(a, b) {
+    return b.length - a.length;
+  });
+  return sorted[0];
 }
 console.log(longestWord(str));
-module.exports=MaxWordLength;
+module.exports = MaxWordLength;
+//lucas added some shit
