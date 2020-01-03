0b1cd8426309bdda8371d402e12877cff2803a1b
lucasbarrosomk6 <lucasbarrosomk6@gmail.com>
2020-01-03 18:06:53 -0500

    added header component and links to home, contact and shop

--- Code Projects/crwn-clothing/src/App.js
+++ Code Projects/crwn-clothing/src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import Contact from "./pages/contactpage/contact.component";
 import "./pages/homepage/homepage.styles.scss";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     </div>
   );
--- Code Projects/crwn-clothing/src/pages/shop/shop.component.jsx
+++ Code Projects/crwn-clothing/src/pages/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import SHOP_DATA from "./shop.data.js";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
     this.state = {
       collections: SHOP_DATA
