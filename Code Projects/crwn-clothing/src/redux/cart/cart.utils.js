182ae995baf73fefd1bfd9317415fb5966c676d3
lucasbarrosomk6 <lucasbarrosomk6@gmail.com>
2020-01-11 14:37:11 -0500

    added functionality to change quantity of items in checkout, if quantity is = 1, the item is removed

--- Code Projects/crwn-clothing/src/redux/cart/cart.utils.js
+++ Code Projects/crwn-clothing/src/redux/cart/cart.utils.js
@@ -16,12 +16,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 }; //this function is where we add the quantity prop to our cardItem.
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
-    //if there is a match, then existingCartItem will exist.
     cartItem => cartItem.id === cartItemToRemove.id
   );
-  if (existingCartItem) {
-    //if the cart item exists, then we filter it out
+  if (existingCartItem.quantity === 1) {
+    //if the quantity is 1, remove it
     return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
   }
-  return cartItems;
+  return cartItems.map(
+    cartItem =>
+      cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem //otherwise decrease the quantity by 1.
+  );
 };
